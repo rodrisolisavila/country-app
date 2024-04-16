a7723eb1a1953babc2bdb970c6a6b3e15c5a2032
rodrisolisavila <rodri.solis.avila@gmail.com>
2024-04-15 23:15:37 -0600

    show country information and switchmap

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'countries-by-country-page',
   templateUrl: './by-country-page.component.html',
-  styles: ``
+  styles: [
+    `img {
+      width: 25px;
+    }`
+  ]
 })
 export class ByCountryPageComponent {
+  
+  public countries: Country[] = [];
 
+  constructor(private countriesService:CountriesService){}
+
+  searchByCountry(term: string): void {
+    this.countriesService.searchCountry(term)
+    .subscribe( countries =>  {
+      this.countries = countries;
+    });
+  }
 }
--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
-  styles: ``
+  styles: [
+    `img {
+      width: 25px;
+    }`
+  ]
 })
 export class ByRegionPageComponent {
+  public countries: Country[] = [];
+
+  constructor(private countriesService:CountriesService){}
+
+  searchByRegion(region: string): void {
+    this.countriesService.searchRegion(region)
+    .subscribe( countries =>  {
+      this.countries = countries;
+    });
+  }
 
 }
